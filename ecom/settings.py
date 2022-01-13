88621925000e25f6e1904e6e163c1ca3721fe709
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-13 14:19:24 +0500

    change settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2velz7248(eb_!+jr=hl@5ni8qcn(h**m5+upzi+*yizkd9+k&'
+SECRET_KEY = '2velz7248(eb_!+jr=hl@5ni8qcn(h**m5+upzi+*yizkd9+k&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,6 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 # Default primary key field type
