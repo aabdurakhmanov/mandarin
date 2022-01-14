c78d2ba2312a7c7dc7e7b382f74106e29df9a651
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-14 15:43:27 +0500

    views changes

--- store/views.py
+++ store/views.py
@@ -13,7 +13,6 @@ def cart(request):
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
 	else:
-		#Create empty cart for now for non-logged in user
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0}
 
