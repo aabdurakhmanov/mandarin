39ceb8a0a7bd9ad1b363eeebf54102827facf75a
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-16 23:15:38 +0500

    cart js changed

--- store/static/js/cart.js
+++ store/static/js/cart.js
@@ -8,8 +8,7 @@ for (i = 0; i < updateBtns.length; i++) {
 		console.log('USER:', user)
 
 		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
-
+			addCookieItem(productId, action)
 		}else{
 			updateUserOrder(productId, action)
 		}
@@ -25,6 +24,7 @@ function updateUserOrder(productId, action){
 			method:'POST',
 			headers:{
 				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
 			},
 			body:JSON.stringify({'productId':productId, 'action':action})
 		})
@@ -32,6 +32,32 @@ function updateUserOrder(productId, action){
 		   return response.json();
 		})
 		.then((data) => {
-		    console.log('Data:', data)
+		    location.reload()
 		});
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+	location.reload()
 }
