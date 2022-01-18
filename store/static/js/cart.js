2e254c00edfb8567272a73f68cae3c45b9c35b7e
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-19 00:37:14 +0500

    update item st

--- store/static/js/cart.js
+++ store/static/js/cart.js
@@ -26,7 +26,7 @@ function updateUserOrder(productId, action){
 				'Content-Type':'application/json',
 				'X-CSRFToken':csrftoken,
 			},
-			body:JSON.stringify({'productId':productId, 'action':action})
+			body:JSON.stringify({'productId': productId, 'action': action})
 		})
 		.then((response) => {
 		   return response.json();
