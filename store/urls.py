59485dc45dd78eb42c9249167f41739c9bbdd246
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-19 01:02:23 +0500

    update_item added

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name='updateitem'),
 
 ]
