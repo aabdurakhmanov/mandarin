2b482ae0a84cd607dbc8aa1b32eccc71f5f00224
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-20 17:07:39 +0500

    add process_order

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('update_item/', views.updateItem, name='updateitem'),
-    path('cart/', views.processOrder, name='processorder'),
+    path('process_order/', views.processOrder, name='process_order'),
 
 ]
