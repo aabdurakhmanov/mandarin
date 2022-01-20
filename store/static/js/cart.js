01df8fa8b4582373bc3d346e458b76ffbd9bc31c
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-01-20 17:06:02 +0500

    add coockie item

--- store/static/js/cart.js
+++ store/static/js/cart.js
@@ -15,6 +15,29 @@ for (i = 0; i < updateBtns.length; i++) {
 	})
 }
 
+function addCookieItem(productId, action){
+    console.log('Not logged in ..')
+
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity': 1}
+        }else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Remove Item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+}
+
+
 function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
 
